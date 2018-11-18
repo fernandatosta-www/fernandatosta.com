ea6fdc3605e800473efdc76d8aa77bf157b0d8ad
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-11-18 21:10:10 -0200

    menu and menu logic

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+function showMenu() {
+  document.getElementById('mobile-menu').classList.remove('mobile-menu-hidden');
+  document.getElementById('mobile-menu').classList.add('mobile-menu-shown');
+}
+
+function hideMenu() {
+  document.getElementById('mobile-menu').classList.remove('mobile-menu-shown');
+  document.getElementById('mobile-menu').classList.add('mobile-menu-hidden');
+}
+
+window.onload = function() {
+  document.getElementById('menu-open').onclick = showMenu;
+  document.getElementById('menu-close').onclick = hideMenu;
+};
