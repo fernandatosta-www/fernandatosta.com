504a9aa0a6af8855267c6020c5f8e9f137d6fdd3
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-11-23 10:09:51 -0200

    implements parallax effect in js+css

---
+++ js/parallax.js
@@ -0,0 +1,22 @@
+function processParallaxes() {
+  let windowHeight = window.innerHeight;
+  let parallaxers = document.getElementsByClassName('parallax-container');
+
+  for(let i=0; i<parallaxers.length; i++) {
+    let elementTop = parallaxers[i].getBoundingClientRect().top;
+    let elementBottom = parallaxers[i].getBoundingClientRect().bottom;
+
+    if ((elementTop < windowHeight) && (elementBottom > 0)) {
+      let offset = Math.abs(elementTop - windowHeight) - (elementBottom - elementTop);
+      parallaxers[i].style.backgroundPositionY = (-offset / 4) + 'px';
+    }
+  }
+}
+
+window.addEventListener('load', function() {
+  processParallaxes();
+});
+
+window.addEventListener('scroll', function() {
+  processParallaxes();
+});
