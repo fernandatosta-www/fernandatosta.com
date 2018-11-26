392a12ca61e334787aafe107d3af3a6aede05204
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-11-26 01:23:53 -0200

    fix mobile parallax

--- js/parallax.js
+++ js/parallax.js
@@ -1,14 +1,23 @@
 function processParallaxes() {
   let windowHeight = window.innerHeight;
+  let windowWidth = window.innerWidth;
   let parallaxers = document.getElementsByClassName('parallax-container');
 
   for(let i=0; i<parallaxers.length; i++) {
     let elementTop = parallaxers[i].getBoundingClientRect().top;
     let elementBottom = parallaxers[i].getBoundingClientRect().bottom;
+    let elementHeight = (elementBottom - elementTop);
 
     if ((elementTop < windowHeight) && (elementBottom > 0)) {
-      let offset = Math.abs(elementTop - windowHeight) - (elementBottom - elementTop);
-      parallaxers[i].style.backgroundPositionY = (-offset / 2) + 'px';
+      let offset = (windowHeight + elementHeight) - elementBottom;
+      let offsetPct = offset / (windowHeight + elementHeight);
+      let topPosition = 0.5 * offsetPct * elementHeight;
+
+      if(windowWidth > windowHeight) {
+        parallaxers[i].style.backgroundPositionY = -(topPosition) + 'px';
+      } else {
+        parallaxers[i].style.backgroundPositionY = -(topPosition / 2) + 'px';
+      }
     }
   }
 }
