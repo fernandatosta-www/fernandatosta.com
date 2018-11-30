22083292e1f1d6dc0fb8d8bc900ba7be5518ab5f
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-11-30 19:04:02 -0200

    adds some code for phrase input

--- js/interactive-phrases.js
+++ js/interactive-phrases.js
@@ -1,27 +1,31 @@
 const PHRASES = [
-  'mudar seu ambiente',
-  'construir com imaginação',
-  'pensar um outro mundo',
-  'criar com as mãos'
+  'mudar seu ambiente  ',
+  'construir com imaginação  ',
+  'pensar um outro mundo  ',
+  'criar com as mãos  ',
+  'planejar seus próprios móveis  '
 ];
 
 let currentPhraseIndex = 0;
 let currentLetterIndex = 0;
 let currentPhrase = '';
+let inputPhrase = '';
 
 let interactiveElement;
 let interactiveElementInvisible;
+
+let myTimeout;
 
 function fadeInWord() {
   currentPhraseIndex = (currentPhraseIndex + 1) % PHRASES.length;
   interactiveElement.innerHTML = PHRASES[currentPhraseIndex];
   interactiveElement.style.opacity = 1;
-  setTimeout(fadeOutWord, 2000);
+  myTimeout = setTimeout(fadeOutWord, 2000);
 }
 
 function fadeOutWord() {
   interactiveElement.style.opacity = 0;
-  setTimeout(fadeInWord, 1200);
+  myTimeout = setTimeout(fadeInWord, 1200);
 }
 
 function updateWord() {
@@ -31,13 +35,13 @@ function updateWord() {
   interactiveElement.innerHTML = '';
   interactiveElementInvisible.innerHTML = currentPhrase;
   interactiveElement.style.opacity = 1;
-  setTimeout(addLetter, 1000);
+  myTimeout = setTimeout(addLetter, 1000);
 }
 
 function addLetter() {
   if(currentLetterIndex > currentPhrase.length) {
     interactiveElement.style.opacity = 0;
-    setTimeout(updateWord, 1200);
+    myTimeout = setTimeout(updateWord, 1200);
   } else {
     let typedPhrase = currentPhrase.slice(0, currentLetterIndex);
     let invisiblePhrase = currentPhrase.slice(currentLetterIndex);
@@ -45,13 +49,34 @@ function addLetter() {
     interactiveElement.innerHTML = typedPhrase;
     interactiveElementInvisible.innerHTML = invisiblePhrase;
     currentLetterIndex = currentLetterIndex + 1;
-    setTimeout(addLetter, 200);
+    myTimeout = setTimeout(addLetter, 200);
   }
+}
+
+function handleClick() {
+  clearTimeout(myTimeout);
+
+  let i = document.createElement('input');
+  i.classList.add('user-phrase-input');
+  i.setAttribute('maxlength', '20');
+
+  interactiveElement.innerHTML = '';
+  interactiveElement.appendChild(i);
+  interactiveElementInvisible.innerHTML = currentPhrase;
+  interactiveElement.removeEventListener('click', handleClick);
+
+  i.addEventListener('keyup', function() {
+    i.style.width = ((i.value.length + 1) * 48) + 'px';
+    interactiveElementInvisible.innerHTML = currentPhrase.slice(0, currentPhrase.length - i.value.length);
+  });
+
+  i.focus();
 }
 
 window.addEventListener('load', function() {
   interactiveElement = document.getElementsByClassName('parallax-interactive')[0];
   interactiveElementInvisible = document.getElementsByClassName('parallax-interactive-invisible')[0];
-  // setTimeout(fadeInWord, 100);
-  setTimeout(updateWord, 100);
+  myTimeout = setTimeout(updateWord, 100);
+  // myTimeout = setTimeout(fadeInWord, 100);
+  // interactiveElement.addEventListener('click', handleClick);
 });
